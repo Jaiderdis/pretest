e3be38c60308ec92dff51d4d68dcd4ed7af47249
Jaider Garcia <jaiderdis1011@gmail.com>
2022-03-19 11:38:03 -0500

    Update PretestApplicationTests.java
    prueba IsPasswordEqual
    Jaider garcia marulanda

--- src/test/java/com/tdea/pretest/PretestApplicationTests.java
+++ src/test/java/com/tdea/pretest/PretestApplicationTests.java
@@ -2,10 +2,15 @@ package com.tdea.pretest;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class PretestApplicationTests {
 
     @Test
-    void contextLoads() {
+    void isPasswordEqual() {
+
+        boolean respuesta=UserUtils.isPasswordEqual("jaider","jaider");
+        assertTrue(respuesta);
     }
 
 }
